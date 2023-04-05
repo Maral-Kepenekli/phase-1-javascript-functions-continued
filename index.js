f9457e7965648704a8c1bfa69b083e9a2de54593
Maral <maral.mamedovna@gmail.com>
2023-04-06 00:20:13 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+// code your solution here ``
+function saturdayFun(activity = "roller-skate") {
+ return `This Saturday, I want to ${activity}!`;
+}
+
+console.log(saturdayFun());
+
+function mondayWork(activity = "go to the office") {
+ return `This Monday, I will ${activity}.`;
+}
+
+console.log(mondayWork("work from home"));
+
+function wrapAdjective(flair = "*") {
+ const innerFunction = function (adjective = "special") {
+  return `You are ${flair}${adjective}${flair}!`
+ };
+ return innerFunction;
+}
+wrapAdjective("||")("a dedicated programmer");
